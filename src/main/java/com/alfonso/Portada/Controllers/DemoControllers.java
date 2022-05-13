acba500cde8a56f2c6c795fbe3ad335ca2179e13
Ponch <Ponch@LAPTOP-PJM34VID>
2022-05-13 16:24:37 -0500

    por

--- src/main/java/com/alfonso/Portada/Controllers/DemoControllers.java
+++ src/main/java/com/alfonso/Portada/Controllers/DemoControllers.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DemoControllers {
 
-	@GetMapping("/")
+	@GetMapping("/portada")
 	public String Portada() {
 		return "Portada";
 	}
